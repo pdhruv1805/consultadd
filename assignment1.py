41921b4b22d0eb532bb634f12aaa32e5914157dc
pdhruv1805 <you@example.com>
2021-03-13 12:38:58 -0500

    git push origin master

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,38 @@
 digit1 = 10; digit2 = 10.5; digit3 = 'three'
-print(digit1)
-print(digit2)
-print(digit3)
+print(digit1,digit2,digit3)
 
-#ans2
+
+#ans3
+x=4
+y=8.9
+
+print('before swap=',x,y)
+
+z=x
+x=y
+y=z
+print('x',x,'y',y)
+
+
+#ans4
+value=input('enter input:')
+print('you entered '+value)
+
+#ans5
+int1=input('enter a number:')
+int2=input('enter a number:')
+
+int3=int(int1)+int(int2)
+
+ans=int3+30
+print(ans)
+
+#ans6
+val=input('enter value ')
+print('the data type of variable is',type(val))
+
+#ans7
+val=input('enter a word ')
+print('enter value is in lower',val.lower())
+print('enter value is in upper',val.upper())
+
